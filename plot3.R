8d887d0ee1a82a522957585cfd860e948239c1b6
smithdanw <dan@innovationsmith.net>
2016-06-30 14:32:53 -0400

    cleanup

--- plot3.R
+++ plot3.R
@@ -4,14 +4,12 @@ library(plyr)
 power <- read.table("household_power_consumption.txt", sep = ";", header = T, stringsAsFactors = F, na.strings = "?")
 power <- mutate(power, Date = dmy(Date))
 power <- filter(power, Date == ymd("2007-02-01") | Date == ymd("2007-02-02"))
-power <- mutate(power, DateTime = paste(Date, Time, sep = "-"), type="o")
-power <- mutate(power, DateTimeObject = ymd_hms(DateTime), type="o")
-power <- mutate(power, DateTimeSeconds = unclass(as.POSIXct(DateTimeObject)))
+power <- mutate(power, DateTimeSeconds = as.POSIXct(paste(Date, Time), format = "%Y-%m-%d %H:%M:%S"))
 
 min <- min(power$DateTimeSeconds)
 max <- max(power$DateTimeSeconds)
 fri <- min +  ((max - min) / 2)
-par(xaxt = "n", mfrow = c(1,1), bg = "transparent", oma = c(0,0,0,0), mar = c(5.1, 4.1, 4.1, 2.1))
+par(mfrow = c(1,1), bg = "transparent", oma = c(0,0,0,0), mar = c(5.1, 4.1, 4.1, 2.1))
 
 plot(power$DateTimeSeconds, y = power$Sub_metering_1, type = "l", pch = 20, xlim = c(min, max), ylab = "Energy sub metering", xlab = "")
 lines(power$DateTimeSeconds, y = power$Sub_metering_2, col = "red")
@@ -19,8 +17,6 @@ lines(power$DateTimeSeconds, y = power$Sub_metering_3, col = "blue")
 
 width = strwidth("Sub_metered_1") * 3
 legend("topright", border = "black", y.intersp = 2,  legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lwd = 1, text.width = width)
-par(xaxt = "l")
-axis(1, labels = c("Thu", "Fri", "Sat"), at = c(min, fri, max), tick = T)
 
 screen <- dev.cur()
 png(width = 480, height = 480, filename = "plot3.png")
